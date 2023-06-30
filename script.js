ef9890a1f1c36cfa4c90dbc59763818caf55e80c
AnanyaChoudhary30 <94673533+AnanyaChoudhary30@users.noreply.github.com>
2023-06-30 14:55:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+//function sum(var1=1,var2=3){
+  //return var1+var2;
+//}
+//console.log(sum());
+
+n1=1,n2=2,n3=3;
+if(n1>=n2&&n1>=n3){
+    largest=n1;
+}
+else if(n2>=n3&&n2>=n1){
+    largest=n2;
+}
+else{
+    largest=n3;
+}
+console.log("the largest number is : "+largest);
